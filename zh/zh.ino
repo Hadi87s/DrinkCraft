6102bc25c34977e6aa3e89096acb6b55596ff7b7
Osama Salah <159712268+OsamaSalah-7@users.noreply.github.com>
2025-04-30 16:44:23 +0300

    pumps and secound ir

--- zh/zh.ino
+++ zh/zh.ino
@@ -3,6 +3,16 @@ const int dc1EnablePin = 10; // ENB
 const int dc1Int3Pin = 9;    // IN3
 const int dc1Int4Pin = 8;    // IN4
 
+// Milk Pump Pins (H-Bridge)
+const int pumpEnablePin = 40; // ENB
+const int pumpInt3Pin = 41;   // IN3
+const int pumpInt4Pin = 42;   // IN4
+
+// Cleaning Pump Pins (H-Bridge)
+const int cleaningEnablePin = 43; // ENA
+const int cleaningInt1Pin = 44;   // IN1
+const int cleaningInt2Pin = 45;   // IN2
+
 // DC Motor2 Pins
 const int dc2EnablePin = 32; // ENB
 const int dc2Int3Pin = 33;   // IN3
@@ -15,6 +25,9 @@ const int cupsDirPin = 4;
 // IR Sensor Pin
 const int irSensorPin = 36; // IR sensor connected to pin 36
 
+const int irSensor2Pin = 13; // Second IR sensor
+
+
 // Fruit Stepper Motor Pins
 const int bananaStepPin = 12;
 const int bananaDirPin = 11;
@@ -24,6 +37,11 @@ const int strawberryDirPin = 6;
 
 const int appleStepPin = 2;
 const int appleDirPin = 7;
+// Mixer Pump Pins (H-Bridge)
+const int mixerEnablePin = 37; // ENA
+const int mixerInt1Pin = 38;   // IN1
+const int mixerInt2Pin = 39;   // IN2
+
 
 // Stepper motor settings
 const int initialStepperSteps = 200;
@@ -64,12 +82,29 @@ void setup() {
   // Set IR sensor pin
   pinMode(irSensorPin, INPUT);
 
+  pinMode(irSensor2Pin, INPUT);
+
+
   // Run cup stepper motor
   displayMessage("Running Cup motor...");
   delay(1000);
   runcupStepper(cupsStepPin, cupsDirPin, initialStepperSteps, HIGH);
   delay(1000);
   displayMessage("Cup motor done.");
+  // Set Milk Pump pins
+  pinMode(pumpEnablePin, OUTPUT);
+  pinMode(pumpInt3Pin, OUTPUT);
+  pinMode(pumpInt4Pin, OUTPUT);
+
+  // Set Mixer Pump pins
+  pinMode(mixerEnablePin, OUTPUT);
+  pinMode(mixerInt1Pin, OUTPUT);
+  pinMode(mixerInt2Pin, OUTPUT);
+  // Set Cleaning Pump pins
+  pinMode(cleaningEnablePin, OUTPUT);
+  pinMode(cleaningInt1Pin, OUTPUT);
+  pinMode(cleaningInt2Pin, OUTPUT);
+
 
   // Run DC motors forward
   displayMessage("Running DC Motor 1 forward...");
@@ -86,16 +121,14 @@ void setup() {
 }
 
 void loop() {
-  if (digitalRead(irSensorPin) == LOW && !hasRunAfterIR) { // Object detected
+
+  // Original IR sensor logic
+  if (digitalRead(irSensorPin) == LOW && !hasRunAfterIR) {
     hasRunAfterIR = true;
     stopDCMotor1();
 
     displayMessage("Object detected. Stopping DC motors.");
     
-   // stopDCMotor2();
-
-   // delay(1000);  // Optional pause after stopping motors
-
     displayMessage("Running Banana Stepper...");
     runStepper(bananaStepPin, bananaDirPin, initialStepperSteps, HIGH);
     delay(2000);
@@ -104,30 +137,47 @@ void loop() {
     runStepper(strawberryStepPin, strawberryDirPin, initialStepperSteps, HIGH);
     delay(2000);
 
-    //  Start both DC motors BEFORE final stepper
-    displayMessage("Starting both DC motors...");
-    runDCMotor1Forward();
-    runDCMotor2Forward();
+    // displayMessage("Starting both DC motors...");
+    // runDCMotor1Forward();
+    // runDCMotor2Forward();
 
     displayMessage("Running Apple Stepper...");
     runStepper(appleStepPin, appleDirPin, initialStepperSteps, HIGH);
     delay(2000);
 
-    displayMessage("All steppers done. Letting motors run for 5 seconds...");
-    //delay(5000); // Let DC motors continue running
-
+    //displayMessage("All steppers done. Letting motors run for 5 seconds...");
     stopDCMotor1();
-    runDCMotor2Forward();
     delay(4000);
-    stopDCMotor2();
     displayMessage("DC Motors stopped.");
+
+    displayMessage("Running Milk Pump...");
+    runPump();
+    delay(5000);
+    stopPump();
+    displayMessage("Milk Pump stopped.");
+
+    displayMessage("Running Mixer Pump...");
+    runMixerPump();
+    delay(5000);
+    stopMixerPump();
+    displayMessage("Mixer Pump stopped.");
+    runDCMotor1Forward();
+
+      // New IR sensor logic
+    if (digitalRead(irSensor2Pin) == LOW) { // Object detected by second IR sensor
+      displayMessage("Second IR Sensor triggered. Stopping DC Motor 1, Running DC Motor 2...");
+      stopDCMotor1();
+      runDCMotor2Forward();
+      delay(5000);
+      stopDCMotor2();
+    }
   }
-   if (digitalRead(irSensorPin) == HIGH) {
+
+  if (digitalRead(irSensorPin) == HIGH) {
     hasRunAfterIR = false;
   }
-  // Reset the trigger after the object leaves the sensor area
-  
 }
+
 
 
 // CUP stepper motor function
@@ -177,3 +227,37 @@ void stopDCMotor2() {
   digitalWrite(dc2Int4Pin, LOW);
   analogWrite(dc2EnablePin, 0);
 }
+void runPump() {
+  digitalWrite(pumpInt3Pin, LOW);
+  digitalWrite(pumpInt4Pin, HIGH);
+  analogWrite(pumpEnablePin, 255);
+}
+
+void stopPump() {
+  digitalWrite(pumpInt3Pin, LOW);
+  digitalWrite(pumpInt4Pin, LOW);
+  analogWrite(pumpEnablePin, 0);
+}
+void runMixerPump() {
+  digitalWrite(mixerInt1Pin, LOW);
+  digitalWrite(mixerInt2Pin, HIGH);
+  analogWrite(mixerEnablePin, 255);
+}
+
+void stopMixerPump() {
+  digitalWrite(mixerInt1Pin, LOW);
+  digitalWrite(mixerInt2Pin, LOW);
+  analogWrite(mixerEnablePin, 0);
+}
+void runCleaningPump() {
+  digitalWrite(cleaningInt1Pin, LOW);
+  digitalWrite(cleaningInt2Pin, HIGH);
+  analogWrite(cleaningEnablePin, 255);
+}
+
+void stopCleaningPump() {
+  digitalWrite(cleaningInt1Pin, LOW);
+  digitalWrite(cleaningInt2Pin, LOW);
+  analogWrite(cleaningEnablePin, 0);
+}
+
